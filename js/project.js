f60bc7774287b7267615f4a2daed0bfa8a64dce6
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2016-10-30 19:03:02 -0400

    Preload
    fix in js and html

--- js/project.js
+++ js/project.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-});
+function preloader() {
+	if (document.images) {
+
+		var img1 = new Image();
+
+		img1.src = "../images/profile1.jpg";
+		img1.src = "../images/nyneon001.jpg";
+		img1.src = "../images/bridal001.jpg";
+		img1.src = "../images/wedding001.jpg";
+	}
+
+}
 
 document.getElementById("mobile").onclick = function() {
     document.querySelector(".small").classList.toggle("open");
