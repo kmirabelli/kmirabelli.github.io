178066db515377f95bf83d11efa8c264d5223db9
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2016-10-30 18:18:54 -0400

    Site revisions
    Changed text color, social images, altered hover states, and layout.

--- js/project.js
+++ js/project.js
@@ -1,11 +1,3 @@
 document.getElementById("mobile").onclick = function() {
     document.querySelector(".small").classList.toggle("open");
 };
-
-$(".carousel a").click(function() {
-    $(".carousel a.current").removeClass("current");
-    $(this).addClass("current");
-    var whichSlide = $(this).index();
-    $(".slide.current").removeClass("current");
-    $(".slide").eq(whichSlide).addClass("current");
-});
